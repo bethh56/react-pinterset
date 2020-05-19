ce1f46c2e6c133c46d3e8ad38827f6842d2331a5
Beth <bethhnielsen56@gmail.com>
2020-05-19 18:58:59 -0500

    wip

---
+++ src/components/BoardContainer/Board/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Board.scss';
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="Board">
+        hi
+      </div>
+    );
+  }
+}
+
+export default Board;
