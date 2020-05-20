f1c4efc27b6ec70b970a48081e46990c5c89cf7e
Beth <bethhnielsen56@gmail.com>
2020-05-19 19:54:01 -0500

    showing single board

--- src/components/BoardContainer/Board/Board.js
+++ src/components/BoardContainer/Board/Board.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Board.scss';
-import boardsData from '../../../helpers/data/boardsData';
 import boardShape from '../../../helpers/propz/boardShape';
 
 class Board extends React.Component {
   static propTypes = {
     board: boardShape.boardShape,
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
+  openSingleBoardEvent = (e) => {
+    e.preventDefault();
+    const { board, setSingleBoard } = this.props;
+    setSingleBoard(board.id);
   }
 
   render() {
@@ -14,11 +21,12 @@ class Board extends React.Component {
     return (
       <div className="Board col-4">
         <div className="card">
-      <div className="card-body">
-    <h5 className="card-title">{board.name}</h5>
-    <p className="card-text">{board.description}</p>
-  </div>
-</div>
+          <div className="card-body">
+            <h5 className="card-title">{board.name}</h5>
+            <p className="card-text">{board.description}</p>
+            <button className="btn btn-dark" onClick={this.openSingleBoardEvent}>View Pins</button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './BoardContainer.scss';
 
 import boardsData from '../../helpers/data/boardsData';
@@ -7,6 +9,10 @@ import authData from '../../helpers/data/authData';
 import Board from './Board/Board';
 
 class BoardContainer extends React.Component {
+  static propTypes = {
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
   state = {
     boards: [],
   }
@@ -14,12 +20,14 @@ class BoardContainer extends React.Component {
   componentDidMount() {
     boardsData.getBoardsByUid(authData.getUid())
       .then((boards) => this.setState({ boards }))
-      .catch((err) => console.error('unable to get all the boards', err));
+      .catch((err) => console.error('unable to get all boards: ', err));
   }
 
   render() {
     const { boards } = this.state;
-    const makeBoards = boards.map((board) => <Board key={board.id} board={board}/>);
+    const { setSingleBoard } = this.props;
+
+    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
 
     return (
       <div className="BoardContainer">
