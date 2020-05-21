e46f3942721176d56180651033e9a4412eb8fc12
Beth <bethhnielsen56@gmail.com>
2020-05-20 19:28:19 -0500

    Player.js is displaying to dom

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -2,11 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './SingleBoard.scss';
+import boardsData from '../../helpers/data/boardsData';
 
 class SingleBoard extends React.Component {
   static propTypes = {
     boardId: PropTypes.string.isRequired,
     setSingleBoard: PropTypes.func.isRequired,
+  }
+
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  componentDidMount() {
+    const { boardId } = this.props;
+    boardsData.getSingleBoard(boardId)
+      .then((request) => {
+        const board = request.data;
+        this.setState({ board });
+      })
+      .catch((err) => console.error('unable to get single board', err));
   }
 
   render() {
